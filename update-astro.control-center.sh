3028c8e629ff09680817baa1df6382eeb58460c3
vladimir shamsheev <shamsheev@MacBook-Pro-vladimir.local>
2015-12-13 00:26:29 +0300

    update-astro.control-center.sh@2.0

--- update-astro.control-center.sh
+++ update-astro.control-center.sh
@@ -1,10 +1,29 @@
-IMAGE_NAME=astro.control-center
+upload=false
 
+while [[ "$1" != "" ]]; do
+    case $1 in
+        --upload )                  upload=true
+                                    ;;
+
+        * )                         echo "error: bad param"
+                                    ;;
+    esac
+    shift
+done
+
+IMAGE_NAME=astro.control-center
 DOCKER_FILE_NAME=Dockerfile-$IMAGE_NAME
 
 cp Dockerfile-common $DOCKER_FILE_NAME
 
 docker build -t svr93/$IMAGE_NAME -f $DOCKER_FILE_NAME .
-echo "svr93/$IMAGE_NAME successfully built!"
 
-docker push svr93/$IMAGE_NAME
+if [[ $? = 0 ]]; then
+
+    echo "svr93/$IMAGE_NAME successfully built!"
+fi
+
+if [[ $upload = true ]]; then
+
+    docker push svr93/$IMAGE_NAME
+fi
