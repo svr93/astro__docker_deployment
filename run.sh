922eae5df9da955744e09065e9fac34418dac3a7
vladimir shamsheev <shamsheev@MacBook-Pro-vladimir-2.local>
2016-01-09 19:33:06 +0300

    run.sh@2.0

--- run.sh
+++ run.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
 
-docker run -it -p 8011:8011 svr93/astro.control-center bash -c \
-"source ~/.bashrc && cd home/astro__server__control_center && node server.js --exchangeData='{}'"
+updateCmd=true
+detached=false
+
+while [[ "$1" != "" ]]; do
+    case $1 in
+        --updateGit )               updateCmd=". update.sh"
+                                    ;;
+
+        --detached )                detached=true
+                                    ;;
+
+        * )                         echo "error: bad param"
+                                    ;;
+    esac
+    shift
+done
+
+docker run -it -p 8011:8011 -d=$detached svr93/astro.control-center bash -c \
+"source ~/.bashrc && \
+cd home/astro__server__control_center && \
+$updateCmd && \
+node server.js --exchangeData='$(cat exchangeData.json)'"
